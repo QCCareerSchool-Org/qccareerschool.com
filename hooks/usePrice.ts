5dc2d877d5f247860cd8f1b199b9df1700072d56
Dave Welsh <dave@qccareerschool.com>
2019-11-25 09:28:25 -0500

    Fixes usePrice because location could be undefined

--- hooks/usePrice.ts
+++ hooks/usePrice.ts
@@ -4,25 +4,29 @@ import { useEffect, useState } from 'react';
 import { getQueryString } from '../functions';
 import { Price } from '../models/price';
 
-export const usePrice = (courses: Array<string | string[]>, countryCode: string, provinceCode: string | null) => {
+export const usePrice = (courses: Array<string | string[]>, countryCode?: string, provinceCode?: string | null) => {
   const [ price, setPrice ] = useState<Price[]>([]);
 
   useEffect(() => {
-    (async () => {
-      const url = 'https://api.qccareerschool.com/prices';
-      const promises = courses.map(c => {
-        const params = { courses: Array.isArray(c) ? c : [ c ], countryCode, provinceCode };
-        const queryString = getQueryString(params);
-        return fetch(`${url}?${queryString}`);
-      });
-      try {
-        const results = await Promise.all(promises);
-        setPrice(await Promise.all<Price>(results.map(r => r.json())));
-      } catch (err) {
-        console.error('Unable to look up prices', err);
-      }
-    })();
+    if (countryCode) {
+      fetchPrices();
+    }
   }, [ countryCode, provinceCode ]);
+
+  async function fetchPrices() {
+    const url = 'https://api.qccareerschool.com/prices';
+    const promises = courses.map(c => {
+      const params = { courses: Array.isArray(c) ? c : [ c ], countryCode, provinceCode };
+      const queryString = getQueryString(params);
+      return fetch(`${url}?${queryString}`);
+    });
+    try {
+      const results = await Promise.all(promises);
+      setPrice(await Promise.all<Price>(results.map(r => r.json())));
+    } catch (err) {
+      console.error('Unable to look up prices', err);
+    }
+  }
 
   return price;
 };
