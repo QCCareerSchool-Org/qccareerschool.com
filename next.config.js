d6ca455d410fa192661b7021abb2a6052f388efa
Dave Welsh <dave@qccareerschool.com>
2019-11-26 14:12:54 -0500

    Service worker builds automatically

--- next.config.js
+++ next.config.js
@@ -2,4 +2,26 @@ const withSass = require('@zeit/next-sass');
 const withCss = require('@zeit/next-css');
 const optimizedImages = require('next-optimized-images');
 const withOffline = require('next-offline');
-module.exports = withSass(withCss(optimizedImages(withOffline())));
+module.exports = withSass(withCss(optimizedImages(withOffline({
+  // generateInDevMode: true,
+  // workboxOpts: {
+  //   swDest: 'static/service-worker.js',
+  //   runtimeCaching: [
+  //     {
+  //       urlPattern: /^https?.*/,
+  //       handler: 'NetworkFirst',
+  //       options: {
+  //         cacheName: 'https-calls',
+  //         networkTimeoutSeconds: 15,
+  //         expiration: {
+  //           maxEntries: 150,
+  //           maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
+  //         },
+  //         cacheableResponse: {
+  //           statuses: [0, 200],
+  //         },
+  //       },
+  //     },
+  //   ],
+  // },
+}))));
