209c49837cedfee91dd49c6482370574652750b6
Dave Welsh <dave@qccareerschool.com>
2019-11-27 13:11:40 -0500

    Adds simple push listener

--- src/sw-push-listener.js
+++ src/sw-push-listener.js
@@ -1,5 +1,12 @@
 'use static';
 
-function foo() {
-  console.log('saldkjaslkdj');
-}
+self.addEventListener('push', event => {
+  if (event.data) {
+    console.log('This push event has data: ', event.data.text());
+  } else {
+    console.log('This push event has no data.');
+  }
+  const promiseChain = self.registration.showNotification('Hello, World.');
+
+  event.waitUntil(promiseChain);
+});
