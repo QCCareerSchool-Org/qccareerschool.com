1d605222fae5aa2ec510ab4b5d89c65fa454d7f3
Dave Welsh <dave@qccareerschool.com>
2019-12-04 08:44:33 -0500

    Changes sitemap generation to synchronous fs

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -1,27 +1,26 @@
 const path = require('path');
-const fs = require('fs').promises;
+const fs = require('fs');
 const xml = require('xml');
 
-async function walk(dir, fileList) {
-  const files = (await fs.readdir(dir)).filter(file => !file.startsWith('_') && !file.startsWith('['));
+function walk(dir, fileList) {
+  const files = fs.readdirSync(dir).filter(file => !file.startsWith('_') && !file.startsWith('['));
   fileList = fileList || [];
-  const promises = files.map(async file => {
-    const stat = await fs.stat(path.join(dir, file));
+  files.forEach(file => {
+    const stat = fs.statSync(path.join(dir, file));
     if (stat.isDirectory()) {
-      fileList = await walk(path.join(dir, file), fileList);
+      fileList = walk(path.join(dir, file), fileList);
     } else if (file.startsWith('index.')) {
       fileList.push(dir);
     } else {
       fileList.push(path.join(dir, file));
     }
   });
-  await Promise.all(promises);
   return fileList;
 }
 
-async function getFiles() {
+function getFiles() {
   const basePath = path.join(__dirname, 'src', 'pages');
-  const pages = await walk(basePath)
+  const pages = walk(basePath)
   return pages.map(p => {
     let returnValue = p;
     if (returnValue.startsWith(basePath)) {
@@ -52,6 +51,6 @@ function createSitemap(basepath, filenames) {
   ]};
 }
 
-getFiles().then(files => {
-  return fs.writeFile(path.join(__dirname, 'public', 'sitemap.xml'), xml(createSitemap('https://www.qccareerschool.com', files)));
-});
+const files = getFiles();
+const filename = path.join(__dirname, 'public', 'sitemap.xml');
+fs.writeFileSync(filename, xml(createSitemap('https://www.qccareerschool.com', files)));
