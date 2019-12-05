b4b28c318a7a947faeeab7ac0d87594bf74589eb
Dave Welsh <dave@qccareerschool.com>
2019-12-04 21:18:32 -0500

    Adds missing functionality to bbcode parser

--- src/bbcode-parser.ts
+++ src/bbcode-parser.ts
@@ -1,9 +1,28 @@
 import XBBCODE from 'xbbcode-parser';
 
+XBBCODE.addTags({
+  b: {
+    openTag: () => '<strong>',
+    closeTag: () => '</strong>',
+  },
+  u: {
+    openTag: () => '<u>',
+    closeTag: () => '</u>',
+  },
+  i: {
+    openTag: () => '<em>',
+    closeTag: () => '</em>',
+  },
+  s: {
+    openTag: () => '<s>',
+    closeTag: () => '</s>',
+  },
+});
+
 export default function parseBBCode(input: string): string {
   const result = XBBCODE.process({
     text: input,
-    removeMisalignedTags: false,
+    removeMisalignedTags: true,
     addInLineBreaks: false,
   });
   return result.html;
