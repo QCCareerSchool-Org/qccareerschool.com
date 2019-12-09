6973ac34fad98f0d26972695a878565552a51898
Dave Welsh <dave@qccareerschool.com>
2019-12-09 09:11:18 -0500

    Fixes additional programs
    Simplifies column order
    Fixes typo with mb-md-0 class

--- src/pages/programs/additional-programs.tsx
+++ src/pages/programs/additional-programs.tsx
@@ -30,15 +30,15 @@ const AdditionalPrograms: NextPage = () => (
     <section>
       <Container>
         <Row>
-          <Col xs={12} md={6} className="order-2 order-md-1">
+          <Col xs={12} md={6} className="mb-4 mb-md-0 order-md-2">
+            <img src={WritingFaculties} alt="person writing a book" className="sideImage" />
+          </Col>
+          <Col xs={12} md={6} className="order-md-1">
             <h3>Winghill School of Writing</h3>
             <p className="mb-4">Let your creativity soar with a number of writing courses that will help you become a published author. Winghill’s courses are extremely flexible and designed to work with you as you turn your ideas into workable manuscripts with the help of your tutor. Choose a course based on your writing genre and goals. Popular courses include the Creative Writing Course, Novel Writing Course, or Screenwriting Course, among others</p>
             <div className="text-center">
               <Button href="https://www.winghill.com/" size="lg" variant="secondary">Learn More</Button>
             </div>
-          </Col>
-          <Col xs={12} md={6} className="mb-4 md-md-0 order-1 order-md-2">
-            <img src={WritingFaculties} alt="person writing a book" className="sideImage" />
           </Col>
         </Row>
       </Container>
@@ -47,15 +47,15 @@ const AdditionalPrograms: NextPage = () => (
     <section className="bg-light">
       <Container>
         <Row>
-          <Col xs={12} md={6} className="order-2 order-md-1">
+          <Col xs={12} md={6} className="mb-4 mb-md-0 order-md-2">
+            <img src={TravelFaculties} alt="couple traveling through a beautiful landscape" className="sideImage" />
+          </Col>
+          <Col xs={12} md={6} className="order-md-1">
             <h3>QC Travel School</h3>
             <p className="mb-4">Travel agents might not be as common these days thanks to cheap online travel sites, however for the clientele will still pay handsomely for the services of a seasoned travel agent who can arrange a fantastic getaway &amp; travel itinerary from start to finish. Learn the tools of the trade and how to make a successful career in this highly competitive industry with the Travel &amp; Tourism Course!</p>
             <div className="text-center">
               <Button href="https://www.qctravelschool.com/" size="lg" variant="secondary">Learn More</Button>
             </div>
-          </Col>
-          <Col xs={12} md={6} className="mb-4 md-md-0 order-1 order-md-2">
-            <img src={TravelFaculties} alt="couple traveling through a beautiful landscape" className="sideImage" />
           </Col>
         </Row>
       </Container>
@@ -64,15 +64,15 @@ const AdditionalPrograms: NextPage = () => (
     <section>
       <Container>
         <Row>
-          <Col xs={12} md={6} className="order-2 order-md-1">
+          <Col xs={12} md={6} className="mb-4 mb-md-0 order-md-2">
+            <img src={StyleFaculties} alt="style professional helping customer" className="sideImage" />
+          </Col>
+          <Col xs={12} md={6} className="order-md-1">
             <h3>QC Style Academy</h3>
             <p className="mb-4">Partnered closely with QC Makeup Academy, the Style academy will introduce you to a different angle to the beauty industry with its Fashion Styling Course. Become a full-time fashion stylist and/or personal shopper, or pair those services to your thriving beauty business to offer full-service solutions to your clients. The possibilities are limitless!</p>
             <div className="text-center">
               <Button href="https://www.qcstyleacademy.com/" size="lg" variant="secondary">Learn More</Button>
             </div>
-          </Col>
-          <Col xs={12} md={6} className="mb-4 md-md-0 order-1 order-md-2">
-            <img src={StyleFaculties} alt="style professional helping customer" className="sideImage" />
           </Col>
         </Row>
       </Container>
