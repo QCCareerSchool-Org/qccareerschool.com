84bd218669cd80aa97f8e3e1b04812d792ffddda
Dave Welsh <dave@qccareerschool.com>
2019-12-10 16:50:16 -0500

    Moves GA to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,9 +6,22 @@ import { LocationProvider } from '../providers/location';
 import { ScreenWidthProvider } from '../providers/screen-width';
 import { ScrollPositionProvider } from '../providers/scroll-position';
 
+import { initGA, logPageView } from '../ga';
+
 import '../style.scss';
 
 export default class QCApp extends App {
+  public componentDidMount() {
+    console.log(window.location.host);
+    if (!window.location.host.startsWith('localhost')) {
+      if (!(window as any).GA_INITIALIZED) {
+        initGA();
+        (window as any).GA_INITIALIZED = true;
+      }
+      logPageView();
+    }
+  }
+
   public render() {
     const { Component, pageProps } = this.props;
     return (
