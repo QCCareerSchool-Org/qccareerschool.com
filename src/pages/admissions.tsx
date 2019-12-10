7e215b3d9ba1400b80d6f48a8ed02905c76c1efa
Dave Welsh <dave@qccareerschool.com>
2019-12-10 16:05:56 -0500

    Removes tailing whitespace

--- src/pages/admissions.tsx
+++ src/pages/admissions.tsx
@@ -40,7 +40,7 @@ const AdmissionsPage: NextPage = () => (
           <Col xs={12} md={6} className="order-2 order-md-1">
             <h3>Private Group Admissions &amp; Discounts</h3>
             <p className="mb-4">If you would like to enroll with QC along with a friend, colleague or family member, you can take advantage of QC’s group admissions discount! Any group of two or more enrolling in any course (you don’t all have to enroll in the same course!) will receive a 25% discount for each individual enrolling. Simply call the school to enroll; this is only available for phone enrollments.  Note that this discount cannot be combined with other offers or promotions.</p>
-            <Link href="/contact"><a className="btn btn-secondary btn-lg">Contact QC</a></Link>                  
+            <Link href="/contact"><a className="btn btn-secondary btn-lg">Contact QC</a></Link>
             </Col>
           <Col xs={12} md={6} className="mb-4 md-md-0 order-1 order-md-2">
             <img src={PrivateGroup} alt="Private Group Discounts" className="sideImage" />
@@ -70,7 +70,7 @@ const AdmissionsPage: NextPage = () => (
           <Col md={10} lg={8} className="offset-md-1 offset-lg-2">
             <h3>Have Questions? We are here to help!</h3>
             <p className="mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vestibulum sagittis augue. Praesent interdum odio a justo tincidunt, at scelerisque urna molestie. Ut euismod venenatis faucibus. Vestibulum auctor sollicitudin faucibus. In hac habitasse platea dictumst. Duis efficitur lectus a nisi tristique, id sollicitudin ex porta. In quis cursus urna. Sed fringilla ipsum vitae massa fermentum accumsan. Morbi a tincidunt justo. Phasellus aliquet at sem non cursus. Fusce risus arcu, imperdiet et iaculis in, vehicula in nulla. Aenean mauris urna, venenatis a egestas in, interdum vel ante. Morbi placerat malesuada malesuada. Duis vel nibh id urna pulvinar dignissim vel et lorem.</p>
-            <Link href="/contact"><a className="btn btn-secondary btn-lg">Contact Us</a></Link>           
+            <Link href="/contact"><a className="btn btn-secondary btn-lg">Contact Us</a></Link>
           </Col>
         </Row>
       </Container>
