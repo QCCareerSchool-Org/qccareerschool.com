83cec40b37a7162a25a7f58be2aa1f048d86fea7
Dave Welsh <dave@qccareerschool.com>
2020-01-14 16:07:51 -0500

    Removes colons from headers on contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -27,10 +27,14 @@ const ContactPage: NextPage = () => {
       />
 
       <section id="first-section" className="text-light">
-        <Container>
-          <h1>Contact</h1>
-          <p className="lead">Whether you have a question about enrolling, are a student looking for help with your account or an assignment, or are a graduate looking to expand your skillset, QC’s team of friendly student advisors are always happy to help. Reach out to us any time!</p>
-        </Container>
+        <div className="container">
+          <div className="row">
+            <div className="col-12 col-md-10 col-lg-8">
+            <h1>Contact</h1>
+            <p className="lead">Whether you have a question about enrolling, are a student looking for help with your account or an assignment, or are a graduate looking to expand your skillset, QC’s team of friendly student advisors are always happy to help. Reach out to us any time!</p>
+            </div>
+          </div>
+        </div>
       </section>
 
       <section className="text-dark text-center">
@@ -38,17 +42,17 @@ const ContactPage: NextPage = () => {
           <Row>
             <Col xs={12} md={4}>
               <IoMdPhonePortrait size={iconSize} />
-              <h3>By Phone:</h3>
+              <h3>By Phone</h3>
               <p>Our student support specialists are available by phone and are always happy to take some time to discuss your career path and goals at <a href={`tel:${telephoneNumber}`} className="text-nowrap">{telephoneNumber}</a>.</p>
             </Col>
             <Col xs={12} md={4}>
               <IoMdMail size={iconSize} />
-              <h3>By Email:</h3>
+              <h3>By Email</h3>
               <p>You are more than welcome to email us at <a href="mailto:info@qccareerschool.com" className="text-nowrap">info@qccareerschool.com</a>. You can be sure to receive a quick and informative reply.</p>
             </Col>
             <Col xs={12} md={4}>
               <IoMdChatbubbles size={iconSize} />
-              <h3>By Live Chat:</h3>
+              <h3>By Live Chat</h3>
               <p>Have a quick question and want a speedy response? Our live chat representatives are there to help!</p>
             </Col>
           </Row>
