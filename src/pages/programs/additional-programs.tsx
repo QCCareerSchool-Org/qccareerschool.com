c8f3fa6b57ac423d3f71b062750ba3824bf714ca
Dave Welsh <dave@qccareerschool.com>
2020-01-16 14:23:18 -0500

    Removes links to "other" schools

--- src/pages/programs/additional-programs.tsx
+++ src/pages/programs/additional-programs.tsx
@@ -43,7 +43,6 @@ const AdditionalProgramsPage: NextPage = () => (
           <Col xs={12} md={6} className="order-md-1">
             <h3>Winghill School of Writing</h3>
             <p className="mb-4">Let your creativity soar with a number of writing courses that will help you become a published author. Winghill’s courses are extremely flexible and designed to work with you as you turn your ideas into workable manuscripts with the help of your tutor. Choose a course based on your writing genre and goals. Popular courses include the Creative Writing Course, Novel Writing Course, or Screenwriting Course, among others</p>
-            <Link href="https://www.winghill.com/"><a className="btn btn-secondary btn-sm">VISIT WINGHILL WRITING SCHOOL</a></Link>
           </Col>
         </Row>
       </Container>
@@ -58,7 +57,6 @@ const AdditionalProgramsPage: NextPage = () => (
           <Col xs={12} md={6} className="order-md-1">
             <h3>QC Travel School</h3>
             <p className="mb-4">Travel agents might not be as common these days thanks to cheap online travel sites, however for the clientele will still pay handsomely for the services of a seasoned travel agent who can arrange a fantastic getaway &amp; travel itinerary from start to finish. Learn the tools of the trade and how to make a successful career in this highly competitive industry with the Travel &amp; Tourism Course!</p>
-            <Link href="https://www.qctravelschool.com/"><a className="btn btn-secondary btn-sm">VISIT QC TRAVEL SCHOOL</a></Link>
           </Col>
         </Row>
       </Container>
@@ -73,7 +71,6 @@ const AdditionalProgramsPage: NextPage = () => (
           <Col xs={12} md={6} className="order-md-1">
             <h3>QC Style Academy</h3>
             <p className="mb-4">Partnered closely with QC Makeup Academy, the Style academy will introduce you to a different angle to the beauty industry with its Fashion Styling Course. Become a full-time fashion stylist and/or personal shopper, or pair those services to your thriving beauty business to offer full-service solutions to your clients. The possibilities are limitless!</p>
-            <Link href="https://www.qcstyleacademy.com/"><a className="btn btn-secondary btn-sm">VISIT QC STYLE ACADEMY</a></Link>
           </Col>
         </Row>
       </Container>
