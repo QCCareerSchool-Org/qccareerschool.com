94e0447a97ad99593521e4ded94d9dcb2d9acc34
Dave Welsh <dave@qccareerschool.com>
2021-06-21 16:07:05 -0400

    Linting fixes

--- polyfills.js
+++ polyfills.js
@@ -4,10 +4,10 @@
 
 // core-js comes with Next.js. So, you can import it like below
 import arrayIncludes from 'core-js/features/array/virtual/includes';
-import stringIncludes from 'core-js/features/string/virtual/includes';
-import startsWith from 'core-js/features/string/virtual/starts-with';
-import repeat from 'core-js/features/string/virtual/repeat';
 import assign from 'core-js/features/object/assign';
+import stringIncludes from 'core-js/features/string/virtual/includes';
+import repeat from 'core-js/features/string/virtual/repeat';
+import startsWith from 'core-js/features/string/virtual/starts-with';
 
 // Add your polyfills
 // This files runs at the very beginning (even before React and Next.js core)
