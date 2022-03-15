4557c2ba67b081c9242585bc1e62284a96a222ad
Dave Welsh <dave@qccareerschool.com>
2022-03-15 11:18:28 -0400

    Add time zones to opening hours

--- src/qcCareerSchoolEducationalOrganization.ts
+++ src/qcCareerSchoolEducationalOrganization.ts
@@ -30,10 +30,10 @@ export const qcCareerSchoolEducationalOrganization: WithContext<EducationalOrgan
       'addressCountry': 'CA',
       'telephone': getTelephoneNumber('CA'),
       'hoursAvailable': [
-        { '@type': 'OpeningHoursSpecification', 'opens': '07:30', 'closes': '21:00', 'dayOfWeek': [ 'Monday', 'Tuesday', 'Wednesday', 'Thursday' ] },
-        { '@type': 'OpeningHoursSpecification', 'opens': '07:30', 'closes': '18:00', 'dayOfWeek': 'Friday' },
-        { '@type': 'OpeningHoursSpecification', 'opens': '12:30', 'closes': '18:30', 'dayOfWeek': 'Saturday' },
-        { '@type': 'OpeningHoursSpecification', 'opens': '17:00', 'closes': '22:00', 'dayOfWeek': 'Sunday' },
+        { '@type': 'OpeningHoursSpecification', 'opens': '07:30-04:00', 'closes': '21:00-04:00', 'dayOfWeek': [ 'Monday', 'Tuesday', 'Wednesday', 'Thursday' ] },
+        { '@type': 'OpeningHoursSpecification', 'opens': '07:30-04:00', 'closes': '18:00-04:00', 'dayOfWeek': 'Friday' },
+        { '@type': 'OpeningHoursSpecification', 'opens': '12:30-04:00', 'closes': '18:30-04:00', 'dayOfWeek': 'Saturday' },
+        { '@type': 'OpeningHoursSpecification', 'opens': '17:00-04:00', 'closes': '22:00-04:00', 'dayOfWeek': 'Sunday' },
       ],
     },
     {
@@ -45,10 +45,10 @@ export const qcCareerSchoolEducationalOrganization: WithContext<EducationalOrgan
       'addressCountry': 'US',
       'telephone': getTelephoneNumber('US'),
       'hoursAvailable': [
-        { '@type': 'OpeningHoursSpecification', 'opens': '07:30', 'closes': '21:00', 'dayOfWeek': [ 'Monday', 'Tuesday', 'Wednesday', 'Thursday' ] },
-        { '@type': 'OpeningHoursSpecification', 'opens': '07:30', 'closes': '18:00', 'dayOfWeek': 'Friday' },
-        { '@type': 'OpeningHoursSpecification', 'opens': '12:30', 'closes': '18:30', 'dayOfWeek': 'Saturday' },
-        { '@type': 'OpeningHoursSpecification', 'opens': '17:00', 'closes': '22:00', 'dayOfWeek': 'Sunday' },
+        { '@type': 'OpeningHoursSpecification', 'opens': '07:30-04:00', 'closes': '21:00-04:00', 'dayOfWeek': [ 'Monday', 'Tuesday', 'Wednesday', 'Thursday' ] },
+        { '@type': 'OpeningHoursSpecification', 'opens': '07:30-04:00', 'closes': '18:00-04:00', 'dayOfWeek': 'Friday' },
+        { '@type': 'OpeningHoursSpecification', 'opens': '12:30-04:00', 'closes': '18:30-04:00', 'dayOfWeek': 'Saturday' },
+        { '@type': 'OpeningHoursSpecification', 'opens': '17:00-04:00', 'closes': '22:00-04:00', 'dayOfWeek': 'Sunday' },
       ],
     },
     {
@@ -60,10 +60,10 @@ export const qcCareerSchoolEducationalOrganization: WithContext<EducationalOrgan
       'addressCountry': 'GB',
       'telephone': getTelephoneNumber('GB'),
       'hoursAvailable': [
-        { '@type': 'OpeningHoursSpecification', 'opens': '12:30', 'closes': '24:00', 'dayOfWeek': [ 'Monday', 'Tuesday', 'Wednesday', 'Thursday' ] },
-        { '@type': 'OpeningHoursSpecification', 'opens': '12:30', 'closes': '23:00', 'dayOfWeek': 'Friday' },
-        { '@type': 'OpeningHoursSpecification', 'opens': '17:30', 'closes': '23:30', 'dayOfWeek': 'Saturday' },
-        { '@type': 'OpeningHoursSpecification', 'opens': '00:00', 'closes': '00:00', 'dayOfWeek': 'Sunday' },
+        { '@type': 'OpeningHoursSpecification', 'opens': '12:30+00:00', 'closes': '24:00+00:00', 'dayOfWeek': [ 'Monday', 'Tuesday', 'Wednesday', 'Thursday' ] },
+        { '@type': 'OpeningHoursSpecification', 'opens': '12:30+00:00', 'closes': '23:00+00:00', 'dayOfWeek': 'Friday' },
+        { '@type': 'OpeningHoursSpecification', 'opens': '17:30+00:00', 'closes': '23:30+00:00', 'dayOfWeek': 'Saturday' },
+        { '@type': 'OpeningHoursSpecification', 'opens': '00:00+00:00', 'closes': '00:00+00:00', 'dayOfWeek': 'Sunday' },
       ],
     },
     {
@@ -75,8 +75,8 @@ export const qcCareerSchoolEducationalOrganization: WithContext<EducationalOrgan
       'addressCountry': 'AU',
       'telephone': getTelephoneNumber('AU'),
       'hoursAvailable': [
-        { '@type': 'OpeningHoursSpecification', 'opens': '09:00', 'closes': '14:00', 'dayOfWeek': 'Monday' },
-        { '@type': 'OpeningHoursSpecification', 'opens': '06:00', 'closes': '14:00', 'dayOfWeek': [ 'Tuesday', 'Wednesday', 'Thursday', 'Friday' ] },
+        { '@type': 'OpeningHoursSpecification', 'opens': '09:00+11:00', 'closes': '14:00+11:00', 'dayOfWeek': 'Monday' },
+        { '@type': 'OpeningHoursSpecification', 'opens': '06:00+11:00', 'closes': '14:00+11:00', 'dayOfWeek': [ 'Tuesday', 'Wednesday', 'Thursday', 'Friday' ] },
         { '@type': 'OpeningHoursSpecification', 'opens': '00:00', 'closes': '00:00', 'dayOfWeek': [ 'Saturday', 'Sunday' ] },
       ],
     },
