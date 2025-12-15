02414b687cef39f22fb3b85f581a7efbecf3c0ab
Dave Welsh <dave@qccareerschool.com>
2025-12-15 14:20:34 -0500

    Removed old config

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,6 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    forceSwcTransforms: true,
-  },
-  images: {
-    remotePatterns: [
-      { hostname: 'studentcenter.qccareerschool.com' },
-      { hostname: 'aeea626a74ffdd96fbcf-77df9cf355bf5239094a1d99115ccf2c.ssl.cf1.rackcdn.com' },
-    ],
-  },
   sassOptions: {
     quietDeps: true, // hide deprecation warnings from node_modules
     // If your sass-loader / Sass version supports it, you can also do:
